fef0b6117297a168f051c131d5047fcfaa95e35e
humanoid01 <82585121+humanoid01@users.noreply.github.com>
2021-11-25 14:21:25 +0100

    Create Correct the mistakes of the character recognition software.js

---
+++ Correct the mistakes of the character recognition software.js
@@ -0,0 +1,20 @@
+/*
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+*/
+function correct(string) {
+  return string
+    .split('')
+    .map((s) =>
+      s === '5' ? (s = 'S') : s === '0' ? (s = 'O') : s === '1' ? (s = 'I') : s
+    )
+    .join('')
+}
